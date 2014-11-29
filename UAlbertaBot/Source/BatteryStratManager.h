08d5d66887fc2a862cd6b7e22b4d787e1b91419e
ryandeforest <rdeforest@hotmail.com>
2014-11-29 16:01:53 -0700

    Create BatteryStratManager.h

---
+++ UAlbertaBot/Source/BatteryStratManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Common.h"
+#include "micromanagement/MicroManager.h"
+#include "micromanagement/MicroUtil.h"
+#include "StrategyManager.h"
+#include "base/BuildingManager.h"
+#include "CombatCommander.h"
+#include "InformationManager.h"
+#include "Squad.h"
+#include "SquadData.h"
+#include "SquadOrder.h"
+
+class BatteryStratManager {
+
+	BWTA::Chokepoint *			wallChokepoint;
+	bool						buildBattery;
+	bool						haveBattery;
+	bool						flagBattery;
+	int							numCannons;
+	int							newx;
+	int							newy;
+	SquadData					defSquad;
+	BWAPI::Unit	*				batteryUnit;
+
+public:
+
+	BatteryStratManager();
+	~BatteryStratManager() {};
+	void				update(const std::set<BWAPI::Unit *> & defenseUnits);
+	BWTA::Chokepoint *	getNearestChokepoint();
+	void				workerScoutDefense();
+	void				customBuild(BWAPI::UnitType b)
+};
