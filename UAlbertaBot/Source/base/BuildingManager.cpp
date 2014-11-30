ccd78556f3f5c83db521b73fe118a483ce4267d6
Mark Galloway <mgallowa@ualberta.ca>
2014-11-30 01:42:42 -0700

    BuildingManager: Air rush specific placement

--- UAlbertaBot/Source/base/BuildingManager.cpp
+++ UAlbertaBot/Source/base/BuildingManager.cpp
@@ -178,9 +178,15 @@ BWAPI::TilePosition BuildingManager::getBuildingLocation(const Building & b)
 		else
 		{
 			// set the building padding specifically
-			//int distance = b.type == BWAPI::UnitTypes::Protoss_Photon_Cannon ? 0 : 1;
-			int distance = b.type == BWAPI::UnitTypes::Protoss_Pylon ? 1 : 0;
-			
+			int distance;
+			if(StrategyManager::Instance().getCurrentStrategy() == StrategyManager::ProtossAirRush) 
+			{
+				distance = b.type == BWAPI::UnitTypes::Protoss_Pylon ? 1 : 0;
+			} else 
+			{
+				distance = b.type == BWAPI::UnitTypes::Protoss_Photon_Cannon ? 0 : 1;
+			}
+
 
 			// whether or not we want the distance to be horizontal only
             bool horizontalOnly = b.type == BWAPI::UnitTypes::Protoss_Citadel_of_Adun ? true : false;
