1f3744ee2b3e78c38dae9d50bed1bd594f549ba9
ryandeforest <rdeforest@hotmail.com>
2014-11-29 20:34:50 -0700

    Update BatteryStratManager.cpp

--- UAlbertaBot/Source/BatteryStratManager.cpp
+++ UAlbertaBot/Source/BatteryStratManager.cpp
@@ -22,11 +22,12 @@ void BatteryStratManager::update(const std::set<BWAPI::Unit *> & defenseUnits)
 	}	
 	
 	numCannons = BWAPI::Broodwar->self()->allUnitCount(BWAPI::UnitTypes::Protoss_Photon_Cannon);
+	numForge = BWAPI::Broodwar->self()->allUnitCount(BWAPI::UnitTypes::Protoss_Forge);
 	flagBattery = false;
 	batteryUnit = NULL;
 
 	//start the custom building for battery strategy after a set time, checks every 2s for update to reduce load
-	if (BWAPI::Broodwar->getFrameCount() >= 6000 && (BWAPI::Broodwar->getFrameCount() % 48 == 0))
+	if (BWAPI::Broodwar->getFrameCount() >= 5000 && (BWAPI::Broodwar->getFrameCount() % 48 == 0))
 	{
 		BOOST_FOREACH(BWAPI::Unit * found, BWAPI::Broodwar->getAllUnits()) {
 			if (found->getPlayer() == BWAPI::Broodwar->self())
@@ -54,7 +55,7 @@ void BatteryStratManager::update(const std::set<BWAPI::Unit *> & defenseUnits)
 			buildBattery = true;
 		}
 		//build cannons near chokepoint every 30s until we have 5
-		else if ((BWAPI::Broodwar->getFrameCount() % 720 == 0) && numCannons < 5)
+		else if ((BWAPI::Broodwar->getFrameCount() % 720 == 0) && numCannons < 5 && numForge > 0)
 		{
 			customBuild(BWAPI::UnitTypes::Protoss_Photon_Cannon);
 		}
@@ -63,12 +64,10 @@ void BatteryStratManager::update(const std::set<BWAPI::Unit *> & defenseUnits)
 	//clear squads
 	defSquad.clearSquadData();
 	UnitVector newSquad;
-	//manager worker defense if scout harassment
-	workerScoutDefense();	
 
 	//manage defense units by either defending chokepoint or going to shield battery for recharge
 	BOOST_FOREACH(BWAPI::Unit * found, defenseUnits) {
-		if (found->getShields() < 25 && haveBattery)
+		if (found->getShields() < 25 && haveBattery && batteryUnit->getEnergy() > 10)
 		{
 			found->follow(batteryUnit);
 		}
@@ -90,46 +89,4 @@ BWTA::Chokepoint* BatteryStratManager::getNearestChokepoint()
 void BatteryStratManager::customBuild(BWAPI::UnitType b)
 {
 	BuildingManager::Instance().addBuildingTask(b, BWAPI::TilePosition(wallChokepoint->getCenter()));
-}
-
-void BatteryStratManager::workerScoutDefense()
-{
-	BOOST_FOREACH(BWTA::Region * myRegion, InformationManager::Instance().getOccupiedRegions(BWAPI::Broodwar->self()))
-	{
-		BWAPI::Position regionCenter = myRegion->getCenter();
-		if (!regionCenter.isValid())
-		{
-			continue;
-		}
-
-		// all of the enemy units in this region
-		std::set<BWAPI::Unit *> enemyUnitsInRegion;
-		BOOST_FOREACH(BWAPI::Unit * enemyUnit, BWAPI::Broodwar->enemy()->getUnits())
-		{
-			if (BWTA::getRegion(BWAPI::TilePosition(enemyUnit->getPosition())) == myRegion)
-			{
-				enemyUnitsInRegion.insert(enemyUnit);
-			}
-		}
-
-		// special case: figure out if the only attacker is a worker, the enemy is scouting
-		if (enemyUnitsInRegion.size() == 1 && (*enemyUnitsInRegion.begin())->getType().isWorker())
-		{
-			// the enemy worker that is attacking us
-			BWAPI::Unit * enemyWorker = *enemyUnitsInRegion.begin();
-
-			// get our worker unit that is mining that is closest to it
-			BWAPI::Unit * workerDefender = WorkerManager::Instance().getClosestMineralWorkerTo(enemyWorker);
-			// grab it from the worker manager
-			WorkerManager::Instance().setCombatWorker(workerDefender);
-
-			// put it into a unit vector
-			UnitVector workerDefenseForce;
-			workerDefenseForce.push_back(workerDefender);
-
-			// make a squad using the worker to defend
-			defSquad.addSquad(Squad(workerDefenseForce, SquadOrder(SquadOrder::Defend, regionCenter, 1000, "Get That Scout!")));
-			return;
-		}
-	}
 }
